7083e3ebfd541214e995e546459c69dd44b236a4
codeMDG <rivoherizo2003@gmail.com>
2025-04-15 16:28:22 +0300

    added: recreate the keystore.kjs file and update build_and_release.yml

--- README.md
+++ README.md
@@ -8,29 +8,34 @@ This mobile app will be used to list all songs of the Malagasy Choir A.Fa.Fi
 sudo apt update && sudo apt install android-sdk
 ```
 
-# Connect device for usb debugging
-- Activate usb debugging for in the device settings<br>
-- Run the following command in the container to check if the device is connected
+## Connect device for usb debugging
+# Activate usb debugging for in the device settings<br>
+# Run the following command in the container to check if the device is connected
     ```bash
     flutter doctor
     ```
-- Run the following command to install dependencies
+# Run the following command to install dependencies
     ```bash
     flutter pub get
     ```
-- Run the following command to install dependencies needed for usb debugging and to launch the emulator on real android device
+# Run the following command to install dependencies needed for usb debugging and to launch the emulator on real android device
     ```bash
     flutter run
     ```
 
 # Release apk
 ```bash
-flutter build apk --obfuscate --split-debug-info=build/app/outputs/symbols
-```
+ flutter clean && flutter pub get && flutter pub run flutter_launcher_icons && flutter build apk --release --split-per-abi
+ ```
 
-# Create a key
+# Create a key to sign apk
 ```bash
 keytool -genkey -v -keystore upload-keystore.jks -keyalg RSA -keysize 2048 -validity 10000 -alias upload
+```
+
+# Output key store
+```bash
+ base64 -w 0 upload-keystore.jks > keystore_base64.txt
 ```
 
 # install dependencies flutter fire
--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,8 +1,15 @@
+import java.util.Properties
+import java.io.FileInputStream
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+}
+
+val keystoreProperties = rootProject.file("key.properties").reader().use { 
+    Properties().apply { load(it) }
 }
 
 android {
@@ -31,20 +38,18 @@ android {
     }
 
     signingConfigs {
-        release {
-            keyAlias keystoreProperties['keyAlias']
-            keyPassword keystoreProperties['keyPassword']
-            storeFile keystoreProperties['storeFile'] ? file(keystoreProperties['storeFile']) : null
-            storePassword keystoreProperties['storePassword']
+        create("release") {
+            keyAlias = keystoreProperties["keyAlias"] as String
+            keyPassword = keystoreProperties["keyPassword"] as String
+            storeFile = keystoreProperties["storeFile"]?.let { file(it) }
+            storePassword = keystoreProperties["storePassword"] as String
         }
     }
 
     buildTypes {
-        release {
+        getByName("release") {
             // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            //signingConfig = signingConfigs.getByName("debug")
-            signingConfig signingConfigs.release
+            signingConfig = signingConfigs.getByName("release")
         }
     }
 }
